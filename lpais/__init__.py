8c625b5a0584075616856c12354cd0ca17b79393
Loïc Peron <peronloic.us@gmail.com>
2018-09-28 16:18:44 -0600

    add lib and tests

---
+++ lpais/__init__.py
@@ -0,0 +1,5 @@
+"""lpais - use libais to decode ais messages, replacing ais.stream"""
+
+__version__ = '0.0.1'
+__author__ = 'Loïc Peron <peronloic.us@gmail.com>'
+__all__ = []
---
+++ tests/test_pep8.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import lpais
+import pep8
+import pathlib
+
+def test_pep8_conformance():
+    """Test that we conform to PEP8."""
+    pep8style = pep8.StyleGuide(prefix='E')
+    base = pathlib.Path(lpais.__file__).parent
+    result = pep8style.check_files(map(str, [
+        base,
+        # base / 'subdir',
+    ]))
+assert result.total_errors == 0
