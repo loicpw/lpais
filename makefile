ec4e9ff78a4bd7c90a085fdfb2e118ba9a6e5bb4
Loïc Peron <peronloic.us@gmail.com>
2018-09-28 16:10:50 -0600

    add makefile, setup and requirements

---
+++ makefile
@@ -0,0 +1,18 @@
+all: install
+
+install: 
+	pip install -r requirements.txt
+	pip install .
+
+dev: 
+	pip install -r requirements-dev.txt
+	pip install -e .
+
+freeze: 
+	pip freeze | sort | grep -v 'lpais'
+test: 
+	cd tests && ( pytest -rXxs -vv --cov-report html -cov-report term-missing --cov lpais )
+
+doc: 
+	cd docs && make html
+
---
+++ setup.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+setuptools.setup(
+    name="lpais",
+    version="0.0.1",
+    url="https://github.com/loicpw/lpais",
+    download_url = "https://github.com/loicpw/lpais.git",
+
+    author="Loïc Peron",
+    author_email="peronloic.us@gmail.com",
+
+    description="use libais to decode ais, replacing ais.stream",
+    long_description='\n\n'.join(
+        open(f, 'rb').read().decode('utf-8')
+        for f in ['README.rst', 'HISTORY.rst']),
+
+    packages=setuptools.find_packages(),
+
+    install_requires=[
+        'libais',
+    ],
+
+    license='MIT License',
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
